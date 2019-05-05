1f789d5bbe8aee271b95d71f0879530a52d67b3a
Brad Walker <bwalker8038@gmail.com>
2019-05-05 13:35:34 -0500

    Adding view-finder component

--- app/components/camera/view-finder.ts
+++ app/components/camera/view-finder.ts
@@ -1,23 +1,35 @@
 import Component from '@ember/component';
 import { debug } from '@ember/debug';
+import { action } from '@ember/object';
 
 export default class CameraViewFinder extends Component {
-  public tagName = 'video'
+  public tagName = ''
 
-  public attributeBindings = ['stream:srcObject'];
-
+  /**
+   * The `MediaStream` for the current device
+   */
   public stream?: MediaStream;
 
+  /**
+   * Boolean flag that determines if the `ViewFinder` has begun it's stream
+   * this flag is needed due to this bug in chrome: https://crbug.com/711524
+   */
   public isStreaming?: Boolean;
 
   /**
-   * @override
+   * Action set's up the media stream. This is fired after the component has been
+   * inserted.
+   *
+   * @param {HTMLVideoElement} element - the video element
+   *
+   * @return {void}
    */
-  public async didInsertElement() {
-    this._super(...arguments);
-
+  @action
+  public async onInsert(
+    element: HTMLVideoElement
+  ) {
     try {
-      await (this.element as HTMLVideoElement).play();
+      await element.play();
       this.set('isStreaming', true);
     } catch (error) {
       debug(error);

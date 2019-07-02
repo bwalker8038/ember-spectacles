491f6ea7a96ad01ae3e8a6fb318fa073413f03af
Brad Walker <bwalker8038@gmail.com>
2019-07-02 16:16:44 -0500

    Adding tests to sensor-toggle, computed props

--- app/components/camera/sensor-toggle.ts
+++ app/components/camera/sensor-toggle.ts
@@ -2,6 +2,7 @@ import Component from '@ember/component';
 import { action, computed } from '@ember/object';
 import { tryInvoke } from '@ember/utils';
 import { debug } from '@ember/debug';
+import { and, gte } from '@ember/object/computed';
 
 export default class CameraSensorToggle extends Component {
   public tagName = '';
@@ -17,6 +18,11 @@ export default class CameraSensorToggle extends Component {
   public trackSettings?: MediaTrackSettings;
 
   /**
+   * current selected sensor index
+   */
+  public sensorIndex = 0;
+
+  /**
    * Function passed from the `<Camera />` component that applies a new
    * `MediaTrackConstraints` object to the current `MediaStreamTrack`
    */
@@ -25,12 +31,40 @@ export default class CameraSensorToggle extends Component {
     constraints: MediaTrackConstraints
   ) => Promise<void>;
 
+  /**
+   * If the number of sensors is two or more, show the toggle
+   */
+  @gte('sensors.length', 2)
+  public hasEnoughSensors?: Boolean;
+
+  @and('hasEnoughSensors', 'trackSettings')
+  public showToggle?: Boolean;
+
+  /**
+   * Getter exposes the currently selected sensor
+   */
+  @computed('sensors', 'sensorIndex')
+  public get currentSensor() {
+    return this.sensors![this.sensorIndex];
+  }
+
+  /**
+   * Action toggles the current selected sensor
+   *
+   * @async
+   * @method toggleSensor
+   *
+   * @return {Promise<void>}
+   */
   @action
   public async toggleSensor() {
-    const [availableDevices] = this.sensors!
-      .filter(({ deviceId }) => this.trackSettings!.deviceId !== deviceId);
+    if (this.sensorIndex < this.sensors!.length) {
+      this.incrementProperty('sensorIndex');
+    } else {
+      this.sensorIndex = 0;
+    }
 
-    const { deviceId } = this.sensors;
+    const { deviceId } = this.currentSensor;
 
     const constraints = {
       deviceId: { exact: deviceId }
@@ -41,6 +75,31 @@ export default class CameraSensorToggle extends Component {
     } catch (error) {
       debug(error);
     }
+  }
+
+  /**
+   * Action sets up the initial state of the component `onInsert`
+   *
+   * @async
+   * @method onInsert
+   * @param {HTMLElement} _el - the mounted element
+   * @param {MediaDeviceInfo[]} sensors
+   *
+   * @return {void}
+   */
+  @action
+  public onInsert(
+    _el: HTMLElement,
+    [sensors]: [MediaDeviceInfo[]]
+) {
+    sensors
+      .forEach(({deviceId}, idx) => {
+        if (this.trackSettings!.deviceId === deviceId) {
+          this.sensorIndex = idx;
+        } else {
+          return;
+        }
+      });
   }
 };
 
--- tests/integration/components/camera/sensor-toggle-test.ts
+++ tests/integration/components/camera/sensor-toggle-test.ts
@@ -1,26 +1,97 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
 
 module('Integration | Component | camera/sensor-toggle', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  hooks.beforeEach(function() {
+    this.updateStreamConstraintsStub = sinon.stub();
 
-    await render(hbs`{{camera/sensor-toggle}}`);
+    this.trackSettings = {
+      aspectRatio: 0.75,
+      colorTemperature: 0,
+      deviceId: "abc123",
+      exposureCompensation: 0,
+      exposureMode: "continuous",
+      exposureTime: 109,
+      facingMode: "environment",
+      focusDistance: 0,
+      focusMode: "continuous",
+      frameRate: 30,
+      groupId: "175f8c9bcc8a9acd423c7188b562f6646424b95706dd1861d14507736086e99a",
+      height: 640,
+      iso: 100,
+      resizeMode: "none",
+      torch: false,
+      whiteBalanceMode: "continuous",
+      width: 480,
+      zoom: 1
+    } as MediaTrackSettings;
+  });
 
-    assert.equal(this.element.textContent.trim(), '');
+  hooks.afterEach(function () {
+    sinon.reset();
+  });
 
-    // Template block usage:
+  test('The sensor toggle component renders', async function (assert) {
+    const sensors = [
+      { deviceId: "abc123" },
+      { deviceId: "def456" },
+      { deviceId: "hij789" },
+    ];
+
+    this.setProperties({ sensors });
+
     await render(hbs`
-      {{#camera/sensor-toggle}}
-        template block text
-      {{/camera/sensor-toggle}}
+      {{camera/sensor-toggle
+        sensors=this.sensors
+        trackSettings=this.trackSettings
+        updateStreamConstraints=this.updateStreamConstraintsStub
+      }}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    const toggle: HTMLButtonElement = this.element.querySelector('.js-toggle-sensor');
+
+    assert.equal(
+      toggle.className === 'js-toggle-sensor',
+      true,
+      'When 2 or more sensors are passed, the toggle renders'
+    );
+
+    await click(toggle);
+
+    assert.equal(
+      this.updateStreamConstraintsStub.calledOnce,
+      true,
+      'onClick, sensor-toggle attempts to trigger `updateStreamConstraints`'
+    );
+
+  });
+
+  test('The sensor toggle component does not render when a single sensor is passed', async function (assert) {
+    const sensors = [
+      { deviceId: "abc123" },
+    ];
+
+    this.setProperties({ sensors });
+
+    await render(hbs`
+      {{camera/sensor-toggle
+        sensors=this.sensors
+        trackSettings=this.trackSettings
+        updateStreamConstraints=this.updateStreamConstraintsStub
+      }}
+    `);
+
+    const toggle: HTMLButtonElement = this.element.querySelector('.js-toggle-sensor');
+
+    assert.equal(
+      toggle === null,
+      true,
+      'When 1 sensor is passed, the toggle does not renders'
+    );
   });
 });
